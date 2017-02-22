c2e57fe4237d15574dd32864a578376e75af1fa3
Seth Broweleit <hello@getsetbro.com>
2017-02-22 03:38:57 -0600

    svg

--- js/contact.js
+++ js/contact.js
@@ -1,8 +1,8 @@
 $(function() {
     'use strict';
-    var $contactUs = $('[name="contactUs"]');
+    var $contactUs = $('.contactUs');
     var $formInfo = $contactUs.find('.formInfo');
-    var $formFields = $contactUs.find('input, textarea');
+    var $formFields = $contactUs.find('input, select, textarea');
     
     //When clicking on Full hide fail/success boxes
     $formFields.focus(function() { $formInfo.html(''); });
@@ -12,34 +12,28 @@ $(function() {
         submitSuccess: function($form, e) {
             e.preventDefault();// prevent default submit behaviour
             $.ajax({
-                url: "//formspree.io/hello@getsetbro.com", 
+                url: "//formspree.ioi/hello@getsetbro.com", 
                 type: "POST",
                 method: "POST",
                 dataType: "json",
                 data: {
-                    name: $form[0][0].value,//$("input#name").val()
-                    phone: $form[0][1].value,//$("input#email").val()
-                    email: $form[0][2].value,//$("input#phone").val()
-                    message: $form[0][3].value,//$("textarea#message").val()
+                    first: $form[0][1].value,//first
+                    last: $form[0][2].value,//last
+                    email: $form[0][3].value,//email
+                    choice: $form[0][4].value,//choice
+                    message: $form[0][5].value,//message
                 },
                 cache: false,
                 success: function() {                   
                     $form.trigger("reset");//clear all fields
-                    // Success message
-                    $formInfo.html(""+
-                        "<div class='alert alert-success alert-dismissible fade show' role='alert'>"+
-                            "<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>"+
-                            "<strong>:)</strong> Your message has been sent."+
-                        "</div>"
-                    );
+                    $('#contactUsModal').modal('hide');
 
                 },
                 error: function() {
                     // Fail message
                     $formInfo.html(""+
-                        "<div class='alert alert-warning alert-dismissible fade show' role='alert'>"+
-                            "<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>"+
-                            "<strong>:(</strong> It seems that my mail server is not responding. Please try again later!"+
+                        "<div class='text-danger'>"+
+                            "<p>It seems that the mail server is not responding. Please try again later!</p>"+
                         "</div>"
                     );
                 },
